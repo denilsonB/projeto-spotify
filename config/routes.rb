1fcc9cf9c9a0e7f069c9f05ac2afe1f906e0f600
denilsonB <denilson1569@hotmail.com>
2022-07-01 22:25:33 -0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'authentication#spotify'
+
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
+
+  resources :playlists
 
   get '/auth/spotify/callback', to: 'authentication#spotify'
 end
