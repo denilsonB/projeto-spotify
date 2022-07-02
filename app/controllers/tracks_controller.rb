5ce52872d896ef92393540ad4beab30b08fb820d
denilsonB <denilson1569@hotmail.com>
2022-07-02 14:05:36 -0300

    update track controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -13,4 +13,41 @@ class TracksController < ApplicationController
     def new
         @track = Track.new
     end
+
+    #POST /tracks
+    def create
+        
+        track = RSpotify::Track.find(track_params[:track_id])
+        
+        if Playlist.find_by(id: track_params[:playlist_id]).tracks.where(id_spotify: track.id).exists?
+            return redirect_to tracks_path
+        else
+            AddTrackJob.perform_now(current_user,track_params[:track_id],track_params[:playlist_id])
+        end
+
+        @service = TrackServices::Create.call(track,track_params[:playlist_id])
+
+        redirect_to tracks_path if @service.success?
+
+    end
+    
+    # DELETE /tracks/{id}
+    def destroy
+        @track = Track.find(params[:id])
+        spotify_track = RSpotify::Track.find(@track.id_spotify)
+        spotify_playlist = RSpotify::Playlist.find_by_id(@track.playlist.id_spotify)
+
+        if @track.status == "on_queue"
+            spotify_playlist.remove_tracks!([spotify_track])
+        end
+
+        @track.destroy
+        redirect_to tracks_path
+
+    end 
+    private 
+
+    def track_params
+        params.require(:track).permit(:track_id, :playlist_id)
+    end
 end
