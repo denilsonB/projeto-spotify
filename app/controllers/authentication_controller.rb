12387b04b8ab2694ee9beb76213d529e750d9c02
denilsonB <denilson1569@hotmail.com>
2022-07-01 22:22:32 -0300

    update login function

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,10 +3,12 @@ class AuthenticationController < ApplicationController
     protect_from_forgery prepend: true
 
     def spotify
-        @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-        current_user.user_hash = @spotify_user.to_hash
-        current_user.save
-        
-        render json: current_user
+        if request.env['omniauth.auth'] 
+            @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+            current_user.user_hash = @spotify_user.to_hash
+            current_user.save
+        else
+            redirect_to '/auth/spotify'
+        end
     end 
 end
