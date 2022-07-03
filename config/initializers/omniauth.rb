21271b4a978370ebce31d8cd76e1f46df242ce10
denilsonB <denilson1569@hotmail.com>
2022-07-02 22:57:37 -0300

    update initializers

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, "41a63d58c2604a8585e49523121b02e3", "b1c5a86e92d74fd58a9c7b5ae9d8bef1", scope: 'user-follow-modify user-read-recently-played playlist-read-collaborative user-read-email playlist-modify-public user-library-modify user-follow-read user-read-currently-playing user-library-read playlist-read-private user-read-private playlist-modify-private '
+  provider :spotify, "41a63d58c2604a8585e49523121b02e3", "b1c5a86e92d74fd58a9c7b5ae9d8bef1", scope: 'user-read-email playlist-modify-public playlist-modify-private user-library-read user-library-modify user-read-recently-played playlist-read-private playlist-read-collaborative'
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get, :put, :delete]
